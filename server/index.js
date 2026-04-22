50b8b69c78d66ec646d53928deab4e50582f0def
suk2005 <sumitkambli16@gmail.com>
2026-04-22 12:44:22 +0530

    Initial commit - Parking System

--- server/index.js
+++ server/index.js
@@ -1,29 +1,30 @@
-const express = require('express');
-const cors = require('cors');
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 
-const connectToMongo = require('./db');
-const authRoutes = require('./routes/authRoutes');
-const bookingRoutes = require('./routes/bookingRoutes'); // ✅
-const userVerification = require('./middlewares/authMiddleware');
+import connectToMongo from "./db.js";
+import authRoutes from "./routes/authRoutes.js";
+import bookingRoutes from "./routes/bookingRoutes.js";
+import userVerification from "./middlewares/authMiddleware.js";
+
+dotenv.config();
 
 const app = express();
 
-// Middlewares
 app.use(express.json());
 app.use(cors());
 
-// connect to database
 connectToMongo();
 
-// Routes
-app.use('/api/auth', authRoutes);
-app.use('/api/bookings', bookingRoutes);  // ✅ Booking APIs
+app.use("/api/auth", authRoutes);
+app.use("/api/bookings", bookingRoutes);
 
-app.get('/api/getuser', userVerification, (req, res) => {
-    res.json({ success: true, username: req.user.username });     // ✅ Booking APIs in index
+app.get("/api/getuser", userVerification, (req, res) => {
+  res.json({ success: true, username: req.user.username });
 });
 
 const PORT = process.env.PORT || 8080;
+
 app.listen(PORT, () => {
-    console.log('Server is running on the PORT', PORT);
+  console.log(`Server running on ${PORT}`);
 });
